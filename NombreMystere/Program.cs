c147bc06f0875f2d04d6a64ee9b1b23d9ea336d7
= <=dialloabdou@hotmail.com>
2023-10-08 18:34:45 +0200

    Nettoyage de la console

--- NombreMystere/Program.cs
+++ NombreMystere/Program.cs
@@ -1,28 +1,44 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine(" BienVenu au Jeu \"NombreMystere!\"");
-const int  NbreMystere = 20;
-int nbSaisie; 
+const int NbreMystere = 20;
+int nbSaisie;
 bool gagne = false;
+List<int> chiffresJoues = new List<int>();
+string resultat = "";
 Console.WriteLine("Prénom du Joueur !");
 string prenom = Console.ReadLine();
 
-while(!gagne)
+while (!gagne)
 {
-       Console.WriteLine($"{prenom} vous pouvez deviner le nombre mystère entre 1 et 30 !");
-       nbSaisie  = int.Parse(Console.ReadLine());
-       if(nbSaisie == NbreMystere)
-       {
+     Console.Clear();
+     Console.Write("Nombre déjà Joué ! ");
+     foreach (var n in chiffresJoues)
+     {
+          Console.Write($" {n} ");
+     }
+     Console.WriteLine("");
+     Console.WriteLine(resultat);
+     Console.WriteLine($"{prenom} vous pouvez deviner le nombre mystère entre 1 et 30 !");
+
+     nbSaisie = int.Parse(Console.ReadLine());
+
+     chiffresJoues.Add(nbSaisie);
+     if (nbSaisie == NbreMystere)
+     {
           Console.WriteLine("vous avez gagné !");
           gagne = true;
-       }else
-       {
-  
-            if(nbSaisie >NbreMystere)
-            {
-                 Console.WriteLine("le nombre mystère est plus petit");
-            }else
-            {
-                Console.WriteLine("le nombre mystere est plus grand");
-            }
-       }
+     }
+     else
+     {
+
+
+          if (nbSaisie > NbreMystere)
+          {
+               resultat = $"le nombre mystère est plus petit que {nbSaisie}";
+          }
+          else
+          {
+              resultat= $"le nombre mystere est plus grand que {nbSaisie}";
+          }
+     }
 }
